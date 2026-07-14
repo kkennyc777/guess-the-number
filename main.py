d4051d5940156220345b176f0c80a371371d1473
Kenny <ken-y7@outlook.com>
2026-07-14 13:22:44 -0600

    Add the variable to the beginning of the code

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 import random
-#Codigo inicial
+#variables
+player_name
+secret_number
+player_guess
+attempts
+failed_attempts
+#Mostrar Bienvenida
 print ("Welcome to Guess the Number!")
+#Pedir Nombre
 player_name = input ("What's your name? ")
 print ("Good Luck " + player_name + "!")
+#Generar Numero Secreto
 number = random.randrange(101)
-print (number)
+#Inicializar Intentos
+#Preguntar el número del jugador
+#Ganó?
+#Dar Pista
+#Actualizar Intentos
+#Repetir
