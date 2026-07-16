a9cbe3c2fe72d1a445a0c30a75be876a207bafb1
Kenny <ken-y7@outlook.com>
2026-07-16 02:23:26 -0600

    Added more timers betwen each return, added a try and except to identify when the user enters an string instead of a number, changed the some messages to be more consistent and make more sense.

--- main.py
+++ main.py
@@ -7,41 +7,63 @@ failed_attempts = 0
 game_won = False
 #Mostrar Bienvenida
 print ("Welcome to Guess the Number!")
-time.sleep (1)
+time.sleep (0.5)
 #Pedir Nombre
 player_name = input ("What's your name? ")
-time.sleep(1)
+time.sleep(0.5)
 print ("Good Luck " + player_name + "!")
+time.sleep(0.5)
+print ("Please choose a number between 0 and 100.")
 #Generar Numero Secreto
 secret_number = random.randrange(101)
 #Loop
-time.sleep(1)
+time.sleep(0.5)
 while attempts > 0 and not game_won:
-    player_guess = int(input ("Guess the number! between 0 and 100: "))
-    if player_guess > 100:
-        failed_attempts +1
+    #Preguntar el número del jugador
+    try:
+        player_guess = int(input ("Your number: "))
+    except ValueError:
+        print("Please enter a valid number")
+        time.sleep(0.5)
+        failed_attempts += 1
         if failed_attempts == 3:
-            attempts -1
+            attempts -= 1
             failed_attempts = 0
-            print ("You have loss an attempt, please input a correct number")
+            print ("You have loss an attempt, please enter a valid number")
+            time.sleep(0.5)
+            print("Remaining attempts: " + str(attempts))
+            time.sleep(0.5)           
+        continue
+    if player_guess > 100:
+        print ("Please enter a valid number.")
+        failed_attempts +=1
+        if failed_attempts == 3:
+            attempts -=1
+            failed_attempts = 0
+            print ("You have loss an attempt, please enter a valid number")
+            time.sleep(0.5)
+            print("Remaining attempts: " + str(attempts))
+            time.sleep(0.5)
+    #Dar Pista
     elif secret_number < player_guess:
+        #Actualizar Intentos
         attempts -=1
         time.sleep(0.5)
         print ("Try a Lower number!")
+        time.sleep(0.5)
         print("Remaining attempts: " + str(attempts))
+        time.sleep(0.5)
+    #Dar Pista
     elif secret_number > player_guess:
+        #Actualizar Intentos
         attempts -=1
         time.sleep (0.5)
         print ("Try a Higher number!")
+        time.sleep(0.5)
         print("Remaining attempts: " + str(attempts))
-    elif secret_number == player_guess:
+        time.sleep(0.5)
+    else:
+        secret_number == player_guess
+        #Ganó?
         game_won = True
         print ("Congratulations! you have won the game.")
-
-
-#Inicializar Intentos
-#Preguntar el número del jugador
-#Ganó?
-#Dar Pista
-#Actualizar Intentos
-#Repetir
