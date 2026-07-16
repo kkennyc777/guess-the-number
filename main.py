0dd9d6183116faf760be7a8c1ae064a91a509701
Kenny <ken-y7@outlook.com>
2026-07-16 01:26:49 -0600

    Added timers to each return, removed the repeated inputs from the elif conditions and added str to conver the remaining attemtps to text as attempts is equal to an integer and could not be concatenated to an string // lines 31 abd 36

--- main.py
+++ main.py
@@ -1,37 +1,40 @@
 import random
+import time
 #variables
 player_name = ""
-secret_number
-player_guess
 attempts = 10
 failed_attempts = 0
 game_won = False
 #Mostrar Bienvenida
 print ("Welcome to Guess the Number!")
+time.sleep (1)
 #Pedir Nombre
 player_name = input ("What's your name? ")
+time.sleep(1)
 print ("Good Luck " + player_name + "!")
 #Generar Numero Secreto
 secret_number = random.randrange(101)
 #Loop
+time.sleep(1)
 while attempts > 0 and not game_won:
-    player_guess = int(input ("Guess the number! between 0 and 100"))
-    if player_guess != int or player_guess < 100:
+    player_guess = int(input ("Guess the number! between 0 and 100: "))
+    if player_guess > 100:
         failed_attempts +1
-        if failed_attempts == 0:
+        if failed_attempts == 3:
             attempts -1
+            failed_attempts = 0
             print ("You have loss an attempt, please input a correct number")
-        print ("Please try with a NUMBER between 0 and 100")
     elif secret_number < player_guess:
-        attempts =-1
-        player_guess = input ("Try a Lower number!")
+        attempts -=1
+        print ("Try a Lower number!")
+        print("Remaining attempts: " + str(attempts))
     elif secret_number > player_guess:
-        attempts =-1
-        player_guess = input ("Tru a Higher number!")
+        attempts -=1
+        print ("Try a Higher number!")
+        print("Remaining attempts: " + str(attempts))
     elif secret_number == player_guess:
         game_won = True
         print ("Congratulations! you have won the game.")
-
 
 
 #Inicializar Intentos
